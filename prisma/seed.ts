2e52e6d83be200aa3e104679215108dd54a3536e
Skynix <romain.ludovic.day@gmail.com>
2023-08-17 21:23:03 +0200

    feat(db): setup database and seed

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const passwordClement = await bcrypt.hash("Clément-33%", 10);
+  const passwordRomain = await bcrypt.hash("Romain-33%", 10);
+
+  await prisma.user.upsert({
+    where: { email: "clement@gmail.com" },
+    update: {},
+    create: {
+      username: "Naara",
+      email: "clement@gmail.com",
+      password: passwordClement,
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: "romain@gmail.com" },
+    update: {},
+    create: {
+      username: "Skynix",
+      email: "romain@gmail.com",
+      password: passwordRomain,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect;
+  })
+  .catch(async (error) => {
+    console.error(error);
+    await prisma.$disconnect;
+    process.exit(1);
+  });
--- server.ts
+++ server.ts
@@ -1,10 +1,9 @@
 import app from "./src";
 
 try {
-    app.listen(8081, () => {
-        console.log(`Server is running on http://localhost:8081`);
-        
-    })
+  app.listen(8081, () => {
+    console.log(`Server is running on http://localhost:8081`);
+  });
 } catch (error) {
-    console.error(error)
+  console.error(error);
 }
