5c3e66c69da82eef333128284fe06919550beeed
Skynix <romain.ludovic.day@gmail.com>
2023-08-17 19:21:12 +0200

    build(*): structure init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # FuelEconomyBack
-FuelEconomyBack
+
+FuelEconomyBack est une simple API pour l'application FuelEconomy. Elle gère la création, modification, suppression et connexion des utilisateurs.
---
+++ server.ts
@@ -0,0 +1,10 @@
+import app from "./src";
+
+try {
+    app.listen(8081, () => {
+        console.log(`Server is running on http://localhost:8081`);
+        
+    })
+} catch (error) {
+    console.error(error)
+}
